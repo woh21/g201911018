7ee332815b64e2e4c4f38197c9ce958b8a3bc030
Shin, Hyunwoo <woh52123@gmail.com>
2025-05-21 09:42:06 +0900

    three

--- src/g201911018/Main.java
+++ src/g201911018/Main.java
@@ -1,7 +1,9 @@
 package g201911018;
 
 public class Main {
-	  public static void main(String[] args) {
-	    System.out.println("신현우");  
-	  }
-	}
+  public static void main(String[] args) {
+    System.out.println("신현우우");  
+    
+    World.run();
+  }
+}
---
+++ src/g201911018/World.java
@@ -0,0 +1,5 @@
+public class World {
+  public static void run() {
+    System.out.println("201911018"); 
+  }
+}
